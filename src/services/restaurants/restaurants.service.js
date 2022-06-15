d6736b95eef1d8f222ca6790a37c33f4370dcb0d
Alexander Marcham <alexander.marcham@gmail.com>
2022-06-15 07:36:44 +0100

    update for new laptop

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -5,7 +5,7 @@ export const restaurantRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
     if (!mock) {
-      reject("No location found!");
+      reject("No location found here!");
     }
     resolve(mock);
   });
